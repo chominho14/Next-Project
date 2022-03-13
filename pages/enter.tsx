34e026ab645b16323a56d520ce68b6521f901b22
chominho14 <chominho14@naver.com>
2022-03-13 22:33:50 +0900

    v0.2.9 (clean code - import Path)

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { cls } from "../libs/client/utils";
-import Button from "../components/button";
-import Input from "../components/input";
+import { cls } from "@libs/client/utils";
+import Button from "@components/button";
+import Input from "@components/input";
 import type { NextPage } from "next";
 import { useForm } from "react-hook-form";
-import useMutation from "../libs/client/useMutation";
+import useMutation from "@libs/client/useMutation";
 
 interface EnterFoprm {
   email?: string;
   phone?: string;
 }
 
-export default function Enter() {
+const Enter: NextPage = () => {
   const [enter, { loading, data, error }] = useMutation("/api/users/enter");
   const { register, handleSubmit, reset } = useForm<EnterFoprm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -132,4 +132,6 @@ export default function Enter() {
       </div>
     </div>
   );
-}
+};
+
+export default Enter;
