dc3927da78b5b0a7bc34e9bcc7671d99081190d5
chominho14 <chominho14@naver.com>
2022-03-16 21:57:07 +0900

    v0.4.5 (Product - Products Detail)

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,35 +21,39 @@ const Home: NextPage = () => {
       <Head>
         <title>Home</title>
       </Head>
-      <div className="flex px-4 flex-col space-y-5">
-        {data?.products?.map(product => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            comments={1}
-            hearts={1}
-          />
-        ))}
-        <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <div className="flex px-4 flex-col space-y-5">
+          {data?.products?.map(product => (
+            <Item
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              comments={1}
+              hearts={1}
             />
-          </svg>
-        </FloatingButton>
-      </div>
+          ))}
+          <FloatingButton href="/products/upload">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </FloatingButton>
+        </div>
+      )}
     </Layout>
   );
 };
