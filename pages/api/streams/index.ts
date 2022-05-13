7822f6ad70d4afe2efe7eb8b0b509332a8fb9f82
chominho14 <chominho14@naver.com>
2022-05-13 19:17:17 +0900

    v0.6.15(Seeding - Stream Data)

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -30,7 +30,10 @@ async function handler(
       stream,
     });
   } else if (req.method === "GET") {
-    const streams = await client.stream.findMany();
+    const streams = await client.stream.findMany({
+      take: 10,
+      skip: 10,
+    });
     res.json({ ok: true, streams });
   }
 }
