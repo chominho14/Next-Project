19e97c9e8e0c9d6c1caad1cacf3645575c58586a
chominho14 <chominho14@naver.com>
2022-06-28 18:14:57 +0900

    v0.9.11(NextJS - getStaticPaths - blog 파일 변수)

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,11 +2,13 @@ import Layout from "@components/layout";
 import { readdirSync, readFileSync } from "fs";
 import matter from "gray-matter";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
   title: string;
   data: string;
   category: string;
+  slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
@@ -18,12 +20,16 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
       <ul>
         {posts.map((post, index) => (
           <div key={index} className="mb-5">
-            <span>{post.title}</span>
-            <div className="text-lg text-red-500">
-              <span>
-                {post.data} / {post.category}
-              </span>
-            </div>
+            <Link href={`/blog/${post.slug}`}>
+              <a>
+                <span>{post.title}</span>
+                <div className="text-lg text-red-500">
+                  <span>
+                    {post.data} / {post.category}
+                  </span>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </ul>
@@ -32,9 +38,12 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
 };
 
 export async function getStaticProps() {
+  // nodeJS 사용하여 blog에 있는 파일들 가져오기
   const blogPosts = readdirSync("./posts").map(file => {
     const content = readFileSync(`./posts/${file}`, "utf-8");
-    return matter(content).data;
+    // md 지우기
+    const [slug, _] = file.split(".");
+    return { ...matter(content).data, slug };
   });
   console.log(blogPosts);
   return {
